58011db4a6515fd5480751c0bc8f02a18a6d5cc4
Your Name <you@example.com>
2022-04-06 01:49:52 +0530

    commit

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-from flask import Flask,render_template,request
-from app.config import LocalDevelopementConfig,ProductionDevelopementConfig
+from app import create_app
 
-
-app =Flask(__name__)
-app.config.from_object(LocalDevelopementConfig)
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-if __name__== "__main__":
-    app.run(
-        host="0.0.0.0"
-    )
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
